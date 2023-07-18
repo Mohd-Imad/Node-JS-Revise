eac05403fc1d3c1e7f903b358f37663431e4fe09
Mohd-Imad <mohdimd123@gmail.com>
2023-07-18 12:18:44 +0530

    Complete authentication

--- MERN-Project/Server/app.js
+++ MERN-Project/Server/app.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const jwt = require('jsonwebtoken')
+const auth = require('./middleware/auth')
+const cookieParser = require('cookie-parser')
 require('./db/myconn')
 const userRouter = require('./router/userRouter')
 
@@ -9,11 +10,38 @@ const app = express()
 let port=process.env.PORT || 7000
 
 app.use(express.json()) //to read form data
+app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
-app.get('/',(request,response)=>{
-   response.send('<h1>Server is running successfully</h1>')
+app.get('/',(req,res)=>{
+   res.send('<h1>Server is running successfully</h1>')
+})
+
+app.get('/secret',auth,(req,res)=>{
+   console.log(req.cookies.newCookie);
+   res.send(`This is a cookie: ${req.cookies.newCookie}`) //to get cookie
+})
+
+app.get('/logout',auth,async (req,res)=>{
+   try{
+
+      //to logout from current device
+      req.user.tokens = req.user.tokens.filter((currentDev)=>{
+         return currentDev.token !== req.token
+      })
+
+      //to logout from all devices
+      // req.user.tokens = []
+
+      res.clearCookie("newCookie")
+      console.log("Logout successful...!");
+
+      await req.user.save()
+      res.send("Cookie deleted")
+   }catch(err){
+      res.status(500).send(err)
+   }
 })
 
 app.use(userRouter)
--- MERN-Project/Server/router/userRouter.js
+++ MERN-Project/Server/router/userRouter.js
@@ -9,29 +9,35 @@ router.post("/users", async (req, res) => {
         const password = req.body.password
         const confirm_password = req.body.confirm_password
 
-        if(password === confirm_password){
+        if (password === confirm_password) {
 
             var registerUser = new Register({
-                fname : req.body.fname,
-                lname : req.body.lname,
-                mobile : req.body.mobile,
-                email : req.body.email,
-                password:password,
-                confirm_password:confirm_password,
+                fname: req.body.fname,
+                lname: req.body.lname,
+                mobile: req.body.mobile,
+                email: req.body.email,
+                password: password,
+                confirm_password: confirm_password,
             })
             console.log("the success part" + registerUser);
-        }else{
-            res.status(400).send({msg:"Passwords are not matching"})
+        } else {
+            res.status(400).send({ msg: "Passwords are not matching" })
         }
-/* 
-        //hashing password
-        const hashPassword = await bcrypt.hash(password,10) */
 
+        //generating JWT
         const token = await registerUser.generateAuthToken()
-        console.log("the token part "  + token);
+        console.log("the token part " + token);
+
+        //adding token to cookie
+        res.cookie("newCookie", token, {
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        })
+        console.log(res.cookie);
+
 
         const createUser = await registerUser.save()
-        res.status(201).send({msg:"User created successful..!", createdUser:createUser })
+        res.status(201).send({ msg: "User created successful..!", createdUser: createUser })
     } catch (e) {
         res.status(400).send(e)
     }
@@ -53,7 +59,7 @@ router.get("/users/:id", async (req, res) => {
         const _id = req.params.id
         const userData = await Register.findById(_id)
         if (!userData) {
-            res.status(404).send({msg:"No User found for the given ID"})
+            res.status(404).send({ msg: "No User found for the given ID" })
         } else {
             res.status(200).send(userData)
         }
@@ -67,19 +73,19 @@ router.patch("/users/:id", async (req, res) => {
     try {
         const _id = req.params.id
         const updateUser = await Register.findByIdAndUpdate(_id, req.body, { new: true })
-        res.status(201).send({msg:"User data updated successfully...!", updateUser:updateUser })
+        res.status(201).send({ msg: "User data updated successfully...!", updateUser: updateUser })
     } catch (e) {
-        res.status(404).send({msg:"No User found with this ID"})
+        res.status(404).send({ msg: "No User found with this ID" })
     }
 })
 
 //Delete user
-router.delete("/users/:id", async (req, res)=>{
-    try{
+router.delete("/users/:id", async (req, res) => {
+    try {
         const deleteUser = await Register.findByIdAndDelete(req.params.id)
-            res.send({msg:"User deleted successfully...!", deletedUser:deleteUser})
-    }catch(e){
-        res.status(404).send({msg:"No user found with this ID"})
+        res.send({ msg: "User deleted successfully...!", deletedUser: deleteUser })
+    } catch (e) {
+        res.status(404).send({ msg: "No user found with this ID" })
     }
 })
 
@@ -89,21 +95,21 @@ router.post("/login", async (req, res) => {
         const email = req.body.email
         const password = req.body.password
 
-        const loginUser = await Register.findOne({email:email})
+        const loginUser = await Register.findOne({ email: email })
 
-        const isMatch = await bcrypt.compare(password,loginUser.password)
+        const isMatch = await bcrypt.compare(password, loginUser.password)
 
         const token = await loginUser.generateAuthToken()
-        console.log("the token part "  + token);
+        console.log("the token part " + token);
 
-        if(isMatch){
+        if (isMatch) {
             res.send("Login success...!")
-        }else{
+        } else {
             res.send("InValid Password...!")
         }
 
-    }catch(e){
-res.status(400).send("InValid login credentials...!")
+    } catch (e) {
+        res.status(400).send("InValid login credentials...!")
     }
 })
 
